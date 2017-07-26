3c57724238792012cbc309afad42373ef1b8f9fb
India Evans <indiaaeevans@gmail.com>
2017-07-25 20:25:36 -0400

    assignments routing and some front end changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -36,5 +36,19 @@ module.exports = function (app) {
     // });
   });
 
+  //route for retrieving all assignments
+  app.get("/api/assignments", function(req, res) {
+    db.Assignment.findAll({ }).then(function(results) { 
+        res.json(results);
+    });
+  });
+
+  // route for saving a new assignment
+  app.post("/api/assignments", function(req, res){
+    db.Assignment.create({
+      assignName: req.body.assignName
+    });
+  });
+
 }
 
