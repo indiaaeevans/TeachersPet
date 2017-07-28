604188e092326a89f26dd3f9e0e006ab6e7556f5
India Evans <indiaaeevans@gmail.com>
2017-07-28 18:36:37 -0400

    added post students function and updated students model to include first and last name

--- app/components/ReactApp.js
+++ app/components/ReactApp.js
@@ -18,25 +18,20 @@ var ReactApp = React.createFactory(React.createClass({
   render: function() {
     return (
       <div>
-
-
-              <ul className="collection">
+        <ul className="collection">
 
               {this.state.students.map((students, i) =>{
                 return (
                   <li key={i} className="collection-item avatar">
                    <a key={`${students.name + i}`} href="#student-modal"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/150px-Placeholder_no_text.svg.png" alt="" className="circle" />
-                    <p key={students.name} className="name">{students.name}</p>
+                    <p key={students.firstName} className="name">{students.firstName}</p>
                     <p key={students.email} className="email">{students.email}</p>
                   </a>
                 </li>
                 ); 
               })}
-
-              </ul>
-
+        </ul>
       </div>
-
     ); 
   }
 })); 
