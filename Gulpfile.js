d584c1dbebfe6c701757b3a6dec4c686725f9543
Eyad Qassem <ejqassem@gmail.com>
2017-07-29 14:18:26 -0400

    updated GulpFile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,23 @@
-var gulp = require('gulp'), 
-browserify = require('gulp-browserify'), 
-babel = require('gulp-babel'); 
+var gulp = require('gulp'),
+  browserify = require('gulp-browserify'),
+  babel = require('gulp-babel');
 
 gulp.task('react-scripts', function() {
-  gulp.src(['app/main.js'])
-  .pipe(browserify({
-    debug: true, 
-    transform: ['reactify']
-  }))
-  .pipe(babel({
-    presets: ['es2015', 'react'], 
-    compact: false 
-  }))
-  .pipe(gulp.dest('./public/assets.js/')); 
-}); 
+  gulp
+    .src(['app/main.js'])
+    .pipe(
+      browserify({
+        debug: true,
+        transform: ['reactify']
+      })
+    )
+    .pipe(
+      babel({
+        presets: ['es2015', 'react'],
+        compact: false
+      })
+    )
+    .pipe(gulp.dest('./public/assets.js/'));
+});
 
-gulp.task('client-scripts', function() {
-  gulp.src(['public/tmp/index.js'])
-  .pipe(babel({
-    presets: ['es2015'],
-    compact: false 
-  }))
-  .pipe(gulp.dest('./public/assets/js')); 
-}); 
-
-gulp.task('default', ['react-scripts','client-scripts']); 
-
+gulp.task('default', ['react-scripts']);
