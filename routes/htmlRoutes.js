27a90a4d22a7167b3e915b7a06de5826ba763eb2
Eyad Qassem <ejqassem@gmail.com>
2017-07-29 16:43:53 -0400

    removed redundant code, added sticky footer to landing page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,15 +10,18 @@ module.exports = function(app) {
   //   // output rendered by react
   //   res.render('index', { reactOutput: reactHtml });
   // });
-
-  app.get('/', function(req, res) {
-    res.render('index');
-  });
-
-  app.get('/class', function(req, res) {
-    // React.renderToString takes your component and generates the markup
-    var reactHtml = ReactDOMServer.renderToString(ReactApp({}));
-    // output rendered by react
-    res.render('classSummary', { reactOutput: reactHtml });
-  });
+  // app.get('/', function(req, res) {
+  //   res.render('index');
+  // });
+  // app.get('/class', function(req, res) {
+  //   // React.renderToString takes your component and generates the markup
+  //   var reactHtml = ReactDOMServer.renderToString(ReactApp({}));
+  //   // output rendered by react
+  //   res.render('classSummary', { reactOutput: reactHtml });
+  // });
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // *****************************
+  // INDIA!!!!!!!!!!!!!!
+  // Commented out code, so authentication routes won't be interfered with by other routes
+  // *****************************
 };
